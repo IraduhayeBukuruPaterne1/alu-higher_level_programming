3c1047411854d478491df657cc8a034bcd5de0e9
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-02-04 19:38:22 +0000

    update

--- 8-concat_edges.py
+++ 8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
-str = str[38:66].strip() + str[105:111] + str[0:6].strip()
-print(str)
+print(str[39:66] + " " + str[107:111] + " " + str[0:6])
