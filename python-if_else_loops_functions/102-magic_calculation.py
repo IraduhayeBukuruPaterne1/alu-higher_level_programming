42a6d3ee9a1647ee97be2d73763db6428d18bbc9
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-02-09 09:30:19 +0000

    update

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,8 @@
-def magic_calculation(a, b):
-    return (98 + a ** b)
-    
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+     if a < b:
+        return c
+        if c > b:
+            return a + 
+            return (a * b) - 
+            
