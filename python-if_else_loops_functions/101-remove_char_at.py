ff34dc4f4bc48e4b65bf4c56c10da233705a227e
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-02-09 09:17:49 +0000

    update

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,2 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    first_part = str[:n]
-     if n < 0:
-        last_part = str[n-0:]
-        else:
-            last_part = str[n+1:]
- total_part = first_part + last_part
- return total_part
- 
+#!/bin/bash
+python3 -c "import py_compile; py_compile.compile('$PYFILE', '$PYFILE' + 'c')"
