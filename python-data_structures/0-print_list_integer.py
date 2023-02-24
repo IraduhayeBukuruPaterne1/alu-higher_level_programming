0725017a67836063d9113fc569675016b1f4645c
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-02-24 12:13:22 +0000

    paterne

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{:d}".format(integer))
+    for each in my_list:
+        print("{:d}".format(my_list[each - 1]))
