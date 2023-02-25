95cb1cdf4c78310a9b7868e68a09a421d00d4cd9
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-02-25 17:43:01 +0000

    hwvdK

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    ret = 0
-    for y in range(0, x):
-        try:
-            print("{:d}".format(my_list[y]), end="")
-            ret += 1
-        except(ValueError, TypeError):
-            continue
+    printed = 0
+    if(my_list):
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end='')
+                printed += 1
+            except(ValueError, TypeError):
+                pass
     print()
-    return 
+    return(printed)
