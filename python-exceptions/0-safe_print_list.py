6f8d6a686d8e432a801f5afce0ff8fe943d0e3af
IraduhayeBukuruPaterne1 <iraduhayebukurupaterne@gmail.com>
2023-02-28 19:16:13 +0200

    akhwfd

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
-    ret = 0
-    for each in range(x):
+    func = 0
+    for i in range(x):
         try:
-            print(f"{my_list[each]}", end="")
-            ret += 1
-        except:
+            print("{}".format(my_list[i]), end="")
+            func += 1
+        except IndexError:
             break
-    print()
-    return (ret)
+    print("")
+    return (func)
