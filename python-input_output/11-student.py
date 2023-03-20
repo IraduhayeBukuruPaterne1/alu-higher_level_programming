7771b5b0d5af8cb0b67e8f1d2de965b759cbf837
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-03-20 07:01:21 +0000

    paterne

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,57 +1,27 @@
 #!/usr/bin/python3
-# class Student that defines a student by: (based on 10-student.py)
-#   - Public instance attributes:
-#       - first_name
-#       - last_name
-#       - age
-#   - Instantiation with first_name, last_name and age:
-#       def __init__(self, first_name, last_name, age)
-#   - Public method def to_json(self):
-#       that retrieves a dictionary representation,
-#       of a Student instance (same as 8-class_to_json.py):
-#           - If attrs is a list of strings, only attribute names,
-#               contained in this list must be retrieved
-#           - Otherwise, all attributes must be retrieved
-#   - Public method def reload_from_json(self, json):
-#       that replaces all attributes of the Student instance:
-#           - You can assume json will always be a dictionary
-#           - A dictionary key will be the public attribute name
-#           - A dictionary value will be the value of the public attribute
-"""
-    class 'Student'
-"""
+"""Defines a class Student."""
 
 
 class Student:
-    """
-        define class 'Student'
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-            init a new Student
-        """
 
+        """Initialize a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-            retrieve dictionary representation of a Student instance
-        """
 
-        if attrs is None or type(attrs) is not list:
-            return self.__dict__
-        for att in attrs:
-            if type(att) is not str:
-                return self.__dict__
-        return {x: self.__dict__[x] for x in self.__dict__ if x in attrs}
+        """Get a dictionary representation of the class Student."""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """
-            replace all attributes of Student instance
-        """
 
-        for key, value in json.items():
-            self.__dict__[key] = 
+        """Replace all attributes of the Student."""
+        for k, v in json.items():
+            setattr(self, k, v)
--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-# function that returns the dictionary description with simple,
-# data structure (list, dictionary, string, integer and boolean),
-# for JSON serialization of an object
-"""
-    define a function 'class_to_json'
-"""
+""" a python class to json function """
 
 
 def class_to_json(obj):
-    """
-        return dictionary description for JSON object
-    """
-
+    """ return a dictionary representaion of a simple data structure """
     return obj.
