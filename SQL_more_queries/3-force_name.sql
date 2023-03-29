fbe4c71d3d769a64ec91ab51037fb29f3712e8fe
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-03-29 09:53:58 +0000

    paterne

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- creates a table
+CREATE TABLE force_name(id INT, name VARCHAR(256) NOT NULL);
+
---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- listing all id with california
+SELECT id, name
+FROM cities
+WHERE state_id IN (
+	SELECT id FROM states
+	WHERE name = 'California')
+ORDER BY id;
+
