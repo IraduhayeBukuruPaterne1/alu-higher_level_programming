126af215203b33d26a48280a2f62e264450569a7
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-06-07 09:55:03 +0000

    paterne

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -6,18 +7,20 @@ module.exports = class Rectangle {
     }
   }
 
-  print (c = 'X') {
+  print () {
     for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-    [this.height, this.width] = [this.width, this.height];
+    const tempWidth = this.width;
+    this.width = this.height;
+    this.height = tempWidth;
   }
 
   double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
+    this.width *= 2;
+    this.height *= 2;
   }
 };
